1249d80727aac1764dc229ca93355e0a50ea5106
kristersCOOL <134638197+kristersCOOL@users.noreply.github.com>
2023-12-06 17:06:01 +0200

    Update game.cs

--- game.cs
+++ game.cs
@@ -6,13 +6,15 @@ namesspace MyGame
  {
    static void Main(string[] args)
     {
-Console.WriteLine(Welcome to a game!)
+       Console.WriteLine("Welcome to a game!");
+       
+       Console.WriteLine("Enter username.");
+       
+       string userName = Console.ReadLine();
 
-Console.WriteLine(Enter username.)
+        Console.WriteLine(userName);
 
-string userName = Console.ReadLine();
 
-Console.WriteLine(userName);
- }
+      }
   }
 }
